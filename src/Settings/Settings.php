2c42358ed6a9964207f04dd53e7d895310329bff
savvy-ben <ben.kubs@havesavvy.com>
2024-01-21 11:21:31 -0600

    APP-230 Update how the results are decoded.

--- src/Settings/Settings.php
+++ src/Settings/Settings.php
@@ -24,8 +24,8 @@ class Settings {
         }
 
         try {
-            return json_decode($this->client->get($endpoint, $defaults != null ? ['x-default-value' => $defaults] : null), true);
-            // return array_map(fn($value): Setting => new Setting($value['key'], '', $value['type'], (object)array($value['type'] => $value['value'])), $results);
+            $results = json_decode($this->client->get($endpoint, $defaults != null ? ['x-default-value' => $defaults] : null), true);
+            return array_map(fn($value): Setting => new Setting($value['setting']['key'], '', $value['setting']['type'], (object)array($value['setting']['type'] => $value['setting']['value'])), $results);
         } catch (InvalidTokenException $e) {
             throw $e;
         } catch (Exception) {
